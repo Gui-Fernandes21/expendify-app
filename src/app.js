274840809b51edec3aea3aae66d8082e88c70e9f
Guilherme Fernandes <guifernandespro@gmail.com>
2023-04-19 14:06:48 -0400

    setting up the project to use client side routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'normalize.css';
-import './styles/styles.scss'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "normalize.css";
+import "./styles/styles.scss";
 
-ReactDOM.render(<p>bla</p>, document.getElementById("app")); 
+const ExpenseDashboardPage = () => <div>Some Text from dashboard</div>;
+
+const AddExpensePage = () => <div>This is my add expense page</div>;
+
+const EditExpensePage = () => <div>This is my EDIT expense page</div>;
+
+const HelpPage = () => <div>This is the HELP page</div>;
+
+const NotFoundPage = () => <div>404! Not Found!</div>;
+
+const routes = (
+	<BrowserRouter>
+		<Switch>
+			<Route path='/' exact component={ExpenseDashboardPage} />
+			<Route path='/create' component={AddExpensePage} />
+			<Route path='/edit' component={EditExpensePage} />
+			<Route path='/help' component={HelpPage} />
+			<Route component={NotFoundPage} />
+		</Switch>
+	</BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
