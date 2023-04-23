d91208d422c5eac37fe38e036b1b00f274b645e3
Guilherme Fernandes <guifernandespro@gmail.com>
2023-04-23 01:36:03 -0400

    organizing project for different routes

--- src/app.js
+++ src/app.js
@@ -1,29 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AppRouter from "./routes/AppRouter";
+
 import "normalize.css";
 import "./styles/styles.scss";
 
-const ExpenseDashboardPage = () => <div>Some Text from dashboard</div>;
-
-const AddExpensePage = () => <div>This is my add expense page</div>;
-
-const EditExpensePage = () => <div>This is my EDIT expense page</div>;
-
-const HelpPage = () => <div>This is the HELP page</div>;
-
-const NotFoundPage = () => <div>404! Not Found!</div>;
-
-const routes = (
-	<BrowserRouter>
-		<Switch>
-			<Route path='/' exact component={ExpenseDashboardPage} />
-			<Route path='/create' component={AddExpensePage} />
-			<Route path='/edit' component={EditExpensePage} />
-			<Route path='/help' component={HelpPage} />
-			<Route component={NotFoundPage} />
-		</Switch>
-	</BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter/>, document.getElementById("app"));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const EditExpensePage = (props) => {
+  console.log(props)
+	return <div>This is my EDIT expense page</div>;
+};
+
+export default EditExpensePage;
